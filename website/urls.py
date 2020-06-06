689d71bad15a95b9e7b931e7928552e683b35514
fasdacombr <flavio_125@hotmail.com>
2020-06-05 21:44:24 -0300

    Adição de novos arquivos
    Adicionado novos arquivos ao projeto e dando um visual a pagina

--- website/urls.py
+++ website/urls.py
@@ -1,11 +1,23 @@
 # Importamos a função index() definida no arquivo views.py
 from django.urls import path
-from . import views
+from website.views import IndexTemplateView, FuncionarioListView, FuncionarioUpdateView, FuncionarioCreateView, FuncionarioDeleteView
 
 app_name = 'website'
 
 # urlpatterns contém a lista de roteamentos de URLs
 urlpatterns = [
     # Get /
-    path('', views.index, name='index'),
+    path('', IndexTemplateView.as_view(), name='index'),
+
+    # Get /funcionario/cadastrar
+    path('funcionario/cadastrar', FuncionarioCreateView.as_view(), name="cadastra_funcionario"),
+
+    # Get /funcionario
+    path('funcionarios/', FuncionarioListView.as_view(), name="lista_funcionarios"),
+
+    # Get/Post /funcionario/{pk}
+    path('funcionario/<pk>', FuncionarioUpdateView.as_view(), name="atualiza_funcionario"),
+
+    # Get/Post /funcionarios/excluir/{pk}
+    path('funcionario/excluir/<pk>', FuncionarioDeleteView.as_view(), name="deleta_funcionario"),
 ]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DeleteView
+from helloworld.models import Funcionario
+from website.forms import InsereFuncionarioForm
 
-# Create your views here.
+# Página Principal.
+# -------------------------------------------------
+
+class IndexTemplateView(TemplateView):
+    template_name = "website/index.html"
+
+
+# Lista de Funcionários
+# -------------------------------------------------
+
+class FuncionarioListView(ListView):
+    template_name = "website/lista.html"
+    model = Funcionario
+    context_object_name = "funcionarios"
+
+# Cadastramento de Funcionarios
+# -------------------------------------------------
+
+class FuncionarioCreateView(CreateView):
+    template_name = "website/cria.html"
+    model = Funcionario
+    form_class = InsereFuncionarioForm
+    success_url = reverse_lazy("website:lista_funcionarios")
+
+# Atualização de Funcionários
+# -------------------------------------------------
+
+class FuncionarioUpdateView(UpdateView):
+    template_name = "website/atualiza.html"
+    model = Funcionario
+    fields = "__all__"
+    context_object_name = "funcionarios"
+    success_url = reverse_lazy("website:lista_funcionarios")
+
+# Exclusão de Funcionários
+# -------------------------------------------------
+
+class FuncionarioDeleteView(DeleteView):
+    template_name = "website/exclui.html"
+    model = Funcionario
+    context_object_name = "funcionarios"
+    success_url = reverse_lazy("website:lista_funcionarios")
